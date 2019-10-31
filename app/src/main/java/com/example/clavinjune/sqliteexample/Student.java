c3108caff5c55c2c3ac710108b241016e39d2f50
Devin Ryan Riota <devinryanriota@gmail.com>
2019-10-31 18:58:01 +0700

    add student email attributes

--- app/src/main/java/com/example/clavinjune/sqliteexample/Student.java
+++ app/src/main/java/com/example/clavinjune/sqliteexample/Student.java
@@ -3,11 +3,19 @@ package com.example.clavinjune.sqliteexample;
 public class Student {
     private String nim, name;
     private Double gpa;
+    private String email;
 
     public Student(String nim, String name, Double gpa) {
         this.nim = nim;
         this.name = name;
         this.gpa = gpa;
+    }
+    
+    public Student(String nim, String name, Double gpa, String email) {
+        this.nim = nim;
+        this.name = name;
+        this.gpa = gpa;
+        this.email = email;
     }
 
     public String getNim() {
@@ -32,5 +40,13 @@ public class Student {
 
     public void setGpa(Double gpa) {
         this.gpa = gpa;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
